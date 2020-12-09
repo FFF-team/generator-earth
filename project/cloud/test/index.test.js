abe0f2b1719fad1fb9007a86d615d5fdbea39a3d
macaihong <macaihong@58.com>
2020-12-09 16:39:45 +0800

    feat: node单元测试 增加uncaughtException

--- project/cloud/test/index.test.js
+++ project/cloud/test/index.test.js
@@ -10,3 +10,25 @@ afterAll(async () => {
     await app && app.close();
 });
 
+describe('test uncaughtException', () => {
+    const error = jest.fn(() => {
+        throw 'the error don`t catch';
+    });
+
+    test('don`t catch error', () => {
+        expect(() => error()).toThrow();
+        expect(error.mock.calls.length).toBe(1);
+    });
+
+    test('catch error', () => {
+        let catchError = () => {
+            try {
+                error();
+            } catch(e) {
+                console.log('catch error msg: ', e);
+            }
+        };
+
+        expect(() => catchError()).not.toThrow();
+    });
+});
